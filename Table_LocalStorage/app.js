787c746e909ad79133936d6dc129ef7ad88ef67c
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2018-10-29 12:14:52 +0100

    save table in the localStorage

--- Table_LocalStorage/app.js
+++ Table_LocalStorage/app.js
@@ -1,14 +1,35 @@
 let table = document.createElement('table');
+table.setAttribute("id", "table");
 let rootDiv = document.getElementById('root');
+
 let addLineBtn = document.createElement('button');
 let addLineBtnText = document.createTextNode("ADD LINE");
+addLineBtn.setAttribute('id', 'addLine');
 addLineBtn.appendChild(addLineBtnText);
 
 let saveBtn = document.createElement('button');
 let saveBtnText = document.createTextNode('SAVE');
+saveBtn.setAttribute("id", "saveBtn");
+saveBtn.appendChild(saveBtnText);
+
+let clearBtn = document.createElement('button');
+let clearBtnText = document.createTextNode('CLEAR');
+clearBtn.appendChild(clearBtnText);
+clearBtn.setAttribute('id', 'clearBtn');
 
 rootDiv.appendChild(table);
 rootDiv.appendChild(addLineBtn);
+rootDiv.appendChild(saveBtn);
+rootDiv.appendChild(clearBtn);
+
+if (localStorage.length > 0) {
+  table.innerHTML = localStorage.getItem("test");
+}
+else {
+  for (let i = 0; i < 4; i++) {
+    addLineToTable();
+  }
+}
 
 function changeCellTxt (elem) {
   elem.addEventListener('click', function() {
@@ -30,38 +51,41 @@ function changeCellTxt (elem) {
 }
 
 function addLineToTable() {
-  let newLine = document.createElement('tr');
+  let newLine = table.insertRow(0);
   for (let i = 0; i < 4; i++) {
-    let newCell = document.createElement('td');
-    const text = document.createTextNode("lorem ipsum");
-    newCell.appendChild(text);
+    let newCell = newLine.insertCell(0);
+    newCell.innerHTML = "lorem ipsum";
     changeCellTxt(newCell);
     newLine.appendChild(newCell);
   }
-  table.insertBefore(newLine, table.children[0]);
   tableColor();
 }
 
 function tableColor() {
-  for (let i = 0; i < table.getElementsByTagName("TR").length; i++) {
-    let line = table.children;
+  for (let i = 0; i < table.children[0].getElementsByTagName("TR").length; i++) {
+    let line = table.children[0].children[0];
     if (i % 2 === 0) {
-      line[i].style.backgroundColor = "grey";
+      line.style.backgroundColor = "grey";
     }
     else {
-      line[i].style.backgroundColor = "white";
+      line.style.backgroundColor = "white";
     }
   }
 }
 
-for (let i = 0; i < 4; i++) {
-  addLineToTable();
-}
+document.querySelectorAll('td').forEach((elem) => {
+  changeCellTxt(elem);
+});
 
-addLineBtn.addEventListener("click", function() {
+
+document.getElementById("addLine").addEventListener("click", function () {
   addLineToTable();
 });
 
-document.querySelectorAll('td').forEach((elem) => {
-  changeCellTxt(elem);
+document.getElementById("clearBtn").addEventListener("click", function() {
+  localStorage.clear();
+});
+
+document.getElementById("saveBtn").addEventListener("click", function () {
+  localStorage.setItem("test", document.getElementById("table").innerHTML);
 });
